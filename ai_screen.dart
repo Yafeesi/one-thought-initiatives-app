87cdeb6b32e9014127711e266436fbd0e8775bbe
Yafeesi <yafeesia@gmail.com>
2026-04-26 12:56:18 +0300

    Create ai_screen.dart

---
+++ ai_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AIScreen extends StatelessWidget {
+  const AIScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("AI Financial Advisor Coming Soon"),
+      ),
+    );
+  }
+}
